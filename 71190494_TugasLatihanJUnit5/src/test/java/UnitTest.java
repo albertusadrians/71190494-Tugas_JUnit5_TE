7daee2027e6943804e6e06279deb9fc989a45ced
albertusadrians <albertus.adrian@ti.ukdw.ac.id>
2022-05-06 20:02:11 +0700

    Revisi 2

--- 71190494_TugasLatihanJUnit5/src/test/java/UnitTest.java
+++ 71190494_TugasLatihanJUnit5/src/test/java/UnitTest.java
@@ -42,39 +42,71 @@ public class UnitTest {
     }
 
     // BVA: Boundary Value Analysis Test Scenario
-    private static Stream<Arguments> bvaTest(){
+    private static Stream<Arguments> bva1Test(){
         return Stream.of(
-                /*
                 // BVA untuk vEC 1 dan vEC 2
                 Arguments.of(true,3999999),
                 Arguments.of(true,4000000),
                 Arguments.of(false,4000001)
-                 */
+        );
+    }
 
+    private static Stream<Arguments> bva2Test(){
+        return Stream.of(
                 // BVA untuk vEC 2 dan vEC 3
                 Arguments.of(true,14999999),
                 Arguments.of(true,15000000),
                 Arguments.of(false,15000001)
+        );
+    }
 
-                /*
+    private static Stream<Arguments> bva3Test(){
+        return Stream.of(
                 // BVA untuk vEC 3 dan vEC 4
                 Arguments.of(true,39999999),
                 Arguments.of(true,40000000),
                 Arguments.of(false,40000001)
-                 */
+        );
+    }
+
+    private static Stream<Arguments> bva4Test(){
+        return Stream.of(
+                Arguments.of(true,999999999998f),
+                Arguments.of(true,999999999999f),
+                Arguments.of(false,1000000000000d)
         );
     }
 
     // Method untuk Boundary Value Analysis Scenario
     @ParameterizedTest
-    @MethodSource("bvaTest")
-    void parameterizedTestPajakBVA(boolean expected, double salary){
+    @MethodSource("bva1Test")
+    void parameterizedTestPajakBVA1(boolean expected, double salary){
         assertNotNull(pajak);
         // BVA untuk vEC 1 dan vEC 2
-        // Assertions.assertEquals(expected,pajak.getPajak(salary)==0);
+        Assertions.assertEquals(expected,pajak.getPajak(salary)==0);
+    }
+
+    @ParameterizedTest
+    @MethodSource("bva2Test")
+    void parameterizedTestPajakBVA2(boolean expected, double salary){
+        assertNotNull(pajak);
         // BVA untuk vEC 2 dan vEC 3
         Assertions.assertEquals(expected,pajak.getPajak(salary)==10);
+    }
+
+    @ParameterizedTest
+    @MethodSource("bva3Test")
+    void parameterizedTestPajakBVA3(boolean expected, double salary){
+        assertNotNull(pajak);
         // BVA untuk vEC 3 dan vEC 4
-        // Assertions.assertEquals(expected,pajak.getPajak(salary)==22);
+        Assertions.assertEquals(expected,pajak.getPajak(salary)==22);
+    }
+
+    @ParameterizedTest
+    @MethodSource("bva4Test")
+    void parameterizedTestPajakBVA4(boolean expected, double salary){
+        assertNotNull(pajak);
+        // BVA untuk vEC 4
+        Assertions.assertEquals(expected,pajak.getPajak(salary)==40f);
     }
 }
